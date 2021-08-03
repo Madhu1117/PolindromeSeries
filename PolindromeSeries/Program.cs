13254820381f9a14fffdb73283db744bf5dda027
nani.madhu1117 <nani.madhu1117@gmail.com>
2021-08-03 10:19:41 +0530

    Add project files.

---
+++ PolindromeSeries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PolindromeSeries
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string s;
+                string rev = "";
+                Console.WriteLine("Enter string");
+                s = Console.ReadLine();
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    rev += s[i].ToString();
+                }
+                if (rev == s)
+                {
+                    Console.WriteLine("Given String is Palindrome\n", s, rev);
+                }
+                else
+                {
+                    Console.WriteLine("Given String is not Palindrome", s, rev);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
